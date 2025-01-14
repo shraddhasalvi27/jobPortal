327de4d0869164f37c837d5da25775131e916e02
shraddha salvi <shraddhasalvi272@gmail.com>
2025-01-14 11:40:50 +0530

    updated index file

--- backend/index.js
+++ backend/index.js
@@ -5,6 +5,10 @@ import cors from "cors";
 //cors are used in cross origin resource share expample one port of frontend to another port of backend
 import connectDB from "./utils/db.js";
 import dotenv from "dotenv";
+import userRoute from "./routes/user.route.js";
+import companyRoute from "./routes/company.route.js";
+import jobRoute from "./routes/job.route.js";
+import applicationRoute from "./routes/application.route.js";
 const app = express();
 dotenv.config({});
 const PORT = process.env.PORT||3001;
@@ -19,6 +23,12 @@ const corsOption = {
     credential: true
 }
 app.use(cors(corsOption));
+
+//api's
+app.use("api/v1/user",userRoute);
+app.use("api/v1/company",companyRoute);
+app.use("api/v1/job",jobRoute);
+app.use("api/v1/application",applicationRoute);
 
 
 app.listen(PORT,()=>{
